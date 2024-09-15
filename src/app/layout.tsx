59a4953ae42fdcd931cfe4e4b3ce93957dc84003
FourSqweez <jeerasak.kr@chomchob.com>
2024-09-15 17:56:31 +0700

    Add : setup local font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
+import localFont from "next/font/local";
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+import "./app.css";
+import Header from "@/components/Header";
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  weight: "100 900",
+  display: "swap",
+  variable: "--font-alpino",
+});
 
 export default function RootLayout({
   children,
@@ -7,8 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={alpino.variable}>
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
