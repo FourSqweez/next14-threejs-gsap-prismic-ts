1a130d43e93a0c9dc8b4fa9e67d1bbc78a39fb13
FourSqweez <jeerasak.kr@chomchob.com>
2024-09-18 13:15:21 +0700

    Add : unused orbitControl

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -94,7 +94,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="hero opacity-0"
     >
-      <View className="hero-scene ffpointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
+      <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
         <Scene />
       </View>
       <div className="grid">
